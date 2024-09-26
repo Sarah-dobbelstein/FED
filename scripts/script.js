31f3dbb82b45b8b7f6acdc12b115b4ba845bfae3
Sarah-dobbelstein <sarah.dobbelstein@hva.nl>
2024-09-26 08:44:41 +0200

    jfeie

--- scripts/script.js
+++ scripts/script.js
@@ -27,59 +27,59 @@ buttons.forEach(button => {
 /* AUTO SCROLLEN van carrousel */
 /*******************************/
 
-const carrousel = document.getElementById('CarrouselAutoScroll');
-const autoScrollInterval = 30; // Tijd in milliseconden tussen het scrollen
-let AutoScrollTimer;
+// const carrousel = document.getElementById('CarrouselAutoScroll');
+// const autoScrollInterval = 30; // Tijd in milliseconden tussen het scrollen
+// let AutoScrollTimer;
 
-  //////////////////////////////////
-  // naar volgende/vorige element //
-  function goToElement(direction) {
-		// het huidige current element opzoeken
-		let currentElement = carrousel.querySelector(":scope > ul > .current");
+//   //////////////////////////////////
+//   // naar volgende/vorige element //
+//   function goToElement(direction) {
+// 		// het huidige current element opzoeken
+// 		let currentElement = carrousel.querySelector(":scope > ul > .current");
 
-		let newElement;
-		if (direction == "previous") {
-			// het nieuwe element is het vorige broertje/zusje
-			newElement = currentElement.previousElementSibling;
-			// checken of nieuwe element bestaat - anders naar laatste
-			if (!newElement) {
-				newElement = carrousel.querySelector(":scope > ul > li:last-of-type");
-			}
-		} else {
-			// het nieuwe element is het volgende broertje/zusje
-			newElement = currentElement.nextElementSibling;
-			// checken of nieuwe element bestaat - anders naar eerste
-			if (!newElement) {
-				newElement = carrousel.querySelector(":scope > ul > li:first-of-type");
-			}
-		}
+// 		let newElement;
+// 		if (direction == "previous") {
+// 			// het nieuwe element is het vorige broertje/zusje
+// 			newElement = currentElement.previousElementSibling;
+// 			// checken of nieuwe element bestaat - anders naar laatste
+// 			if (!newElement) {
+// 				newElement = carrousel.querySelector(":scope > ul > li:last-of-type");
+// 			}
+// 		} else {
+// 			// het nieuwe element is het volgende broertje/zusje
+// 			newElement = currentElement.nextElementSibling;
+// 			// checken of nieuwe element bestaat - anders naar eerste
+// 			if (!newElement) {
+// 				newElement = carrousel.querySelector(":scope > ul > li:first-of-type");
+// 			}
+// 		}
 
-		// naar het nieuwe element scrollen
-		scrollToElement(newElement);
-  }
+// 		// naar het nieuwe element scrollen
+// 		scrollToElement(newElement);
+//   }
 
-function AutoScroll() {
-    // de class "autoScrolling" toevoegen aan de carrousel
-    carrousel.classList.add("autoScrolling");
+// function AutoScroll() {
+//     // de class "autoScrolling" toevoegen aan de carrousel
+//     carrousel.classList.add("autoScrolling");
     
-    // een timer aanzetten
-    AutoScrollTimer = setInterval(function(){
-      // als de timer afgaat naar het volgende element gaan
-      goToElement("next");
-    }, autoScrollInterval);
+//     // een timer aanzetten
+//     AutoScrollTimer = setInterval(function(){
+//       // als de timer afgaat naar het volgende element gaan
+//       goToElement("next");
+//     }, autoScrollInterval);
    
-  }
+//   }
 
-  // Functie om de carrousel op te zetten (je kunt deze uitbreiden)
-function createCarrousel(id) {
-    const carrouselElement = document.getElementById(id);
-    // Hier kun je logica toevoegen om de carrousel op te zetten
-}
+//   // Functie om de carrousel op te zetten (je kunt deze uitbreiden)
+// function createCarrousel(id) {
+//     const carrouselElement = document.getElementById(id);
+//     // Hier kun je logica toevoegen om de carrousel op te zetten
+// }
 
-  (function() {
-    // hier de id gebruiken van de section in de html
-    createCarrousel("CarrouselAutoScroll");
-    AutoScroll();
-    //je kunt hier ook meerdere carrousellen activeren
-  })();
+//   (function() {
+//     // hier de id gebruiken van de section in de html
+//     createCarrousel("CarrouselAutoScroll");
+//     AutoScroll();
+//     //je kunt hier ook meerdere carrousellen activeren
+//   })();
 
